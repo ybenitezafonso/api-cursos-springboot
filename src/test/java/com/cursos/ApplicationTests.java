730bd18792214901508405b65f5fdf67131ebc08
yaiza <yaiza@YaizaAsus.home>
2025-11-04 10:14:03 +0000

    Junit

--- src/test/java/com/cursos/ApplicationTests.java
+++ src/test/java/com/cursos/ApplicationTests.java
@@ -1,13 +1,56 @@
 package com.cursos;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+@AutoConfigureMockMvc
 @SpringBootTest
+@TestMethodOrder(OrderAnnotation.class) //Indica orden a la hora de ejecutar los tests
 class ApplicationTests {
 
+	@Autowired
+	MockMvc mock;
+	
+	@Order(0)
 	@Test
-	void contextLoads() {
+	void eliminarCurso() throws Exception {
+		mock.perform(delete("/curso/Python")).andDo(print());
+	}
+	
+	@Order(1)	
+	@Test
+	void testCursos() throws Exception{
+		mock.perform(get("/cursos")).andDo(print());
 	}
 
+	@Test
+	@Order(2)
+	void testAlta() throws Exception {
+	    mock.perform(post("/curso")
+	        .contentType(MediaType.APPLICATION_JSON)
+	        .content("{\"nombre\":\"Angular 10\",\"duracion\":40,\"horario\":\"tarde\"}")
+	    ).andDo(print());
+	}
+
+	@Test
+	@Order(3)
+	void testActualizacion() throws Exception {
+	    mock.perform(put("/curso")
+	        .contentType(MediaType.APPLICATION_JSON)
+	        .content("{\"nombre\":\"Angular 10\",\"duracion\":80,\"horario\":\"mañana\"}")
+	    ).andDo(print());
+	}
 }
